8cbc167a71a6137d211d151a27b75ec83bfe29cc
bbarr <bbarr1384@gmail.com>
2014-04-04 16:42:08 -0700

    wip

--- app/keywordize.js
+++
@@ -1,8 +0,0 @@
-var _ = require('lodash')
-
-module.exports = function(normalized) {
-  var name = normalized.name || '';
-  var desc = normalized.description || '';
-  normalized.keywords = _.compact((name + ' ' + desc).toLowerCase().split(/\s/).map(function(word) { return word.replace(/(\W)/g, '') }))
-  return normalized
-}
