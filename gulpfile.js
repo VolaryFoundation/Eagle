02065fec4d49cc2fbe08b0df5937aa55c3ee078a
bbarr <bbarr1384@gmail.com>
2014-04-16 15:48:42 -0700

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -21,17 +21,21 @@ gulp.task('build-groups', function() {
   var groupType = require('./app/types/group/group_type')
   db.shortcut('entities')
 
-  db.entities.find({ type: 'group' }, {}, { limit: 10 }).then(function(groups) { 
-    var ps = groups.map(function(group, i) { return db.groups.find({ _entityId: group._id.toString() }) })
-    rsvp.all(ps).then(function(results) {
-      var missing = results.map(function(result, i) {
-        if (!result[0]) {
-          return groups[i]; 
-        }
-      }).filter(function(m) { return m })
-      console.log('warming many ', missing)
-      groupType.warmMany(missing)
-    }, console.log)
-  })
-    
+  function run(skip) {
+    db.entities.find({ type: 'group' }, {}, { limit: 100, skip: skip, timeout: false }).then(function(groups) { 
+      var ps = groups.map(function(group, i) { return db.groups.find({ _entityId: group._id.toString() }) })
+      rsvp.all(ps).then(function(results) {
+        var missing = results.map(function(result, i) {
+          if (!result[0]) {
+            return groups[i]; 
+          }
+        }).filter(function(m) { return m })
+        console.log('warming many ', missing)
+        groupType.warmMany(missing)
+        if (groups.length === 100) run(skip + 100)
+      }, console.log)
+    })
+  }
+
+  run(0)
 })
