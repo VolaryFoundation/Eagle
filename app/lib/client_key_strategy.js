af84cb96ca4bafc0b549eb828a095ea9a8f905d4
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-04-16 21:42:00 -0700

    Removed all console log statment for auth bug

--- app/lib/client_key_strategy.js
+++ app/lib/client_key_strategy.js
@@ -17,18 +17,8 @@ ClientKeyStrategy.prototype.authenticate = function(req, options) {
   var authTimestamp = req.param('authTimestamp')
 
   if (!authId || !authHash || !authTimestamp) return this.fail()
-  console.log(authId)
   db.clients.find({ authId: authId }).then(function(clients) {
-  db.clients
-    console.log(clients[0])
-    console.log("Auth Output")
-    console.log(req.param('authTimestamp'))
-    console.log(req.param('authHash'))
-    console.log("-------------------")
     var client = clients[0]
-    console.log(crypto.createHash('md5').update(authTimestamp).update(client.authSecret).digest('hex'))
-    console.log(crypto.createHash('md5').update(authTimestamp, client.authSecret).digest('hex'))
-
     if (!client || crypto.createHash('md5').update(authTimestamp).update(client.authSecret).digest('hex') != authHash) {
       return this.fail()
     }
