f5562d9977857c816f3f9d5fbc82bd1bafd90111
bbarr <bbarr1384@gmail.com>
2014-04-17 17:38:25 -0700

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -17,22 +17,22 @@ gulp.task('build-groups', function() {
   var _ = require('lodash')
   var db = require('mongo-promise')
   var rsvp = require('rsvp')
-  db.url = process.env.MONGOHQ_URL
+  var config = require('config')
+  db.url = process.env.MONGOHQ_URL || config.servers.mongodb
   var groupType = require('./app/types/group/group_type')
   db.shortcut('entities')
 
-  db.entities.find({ type: 'group' }, {}, { timeout: false }).then(function(groups) { 
-    var ps = groups.map(function(group, i) { return db.groups.find({ _entityId: group._id.toString() }) })
-    rsvp.all(ps).then(function(results) {
-      var missing = results.map(function(result, i) {
-        if (!result[0]) {
-          return groups[i]; 
-        }
-      }).filter(function(m) { return m })
-      console.log('warming many ', missing)
-      groupType.warmMany(missing)
-      console.log('check group length', groups.length)
-    }, console.log)
-  })
+  db.entities.find({}, {}, { timeout: false }).then(function(groups) { 
+    console.log('building groups count ', groups.length)
 
+    console.log(groups.filter(function(group) { console.log(group._id.toString()); return group._id.toString() == '5344863f3779f96e5a920fcb' }))
+
+    db.groups.find({ _entityId: { '$in': _.invoke(_.pluck(groups, '_id'), 'toString') } }, { _entityId: true }).then(function(cached) {
+      console.log('found cached length ', cached.length)
+      var entityIds = _.invoke(_.pluck(cached, '_entityId'), 'toString')
+      var missing = groups.filter(function(group) { return _.contains(entityIds, group._id.toString()) })
+      console.log('warm many length', missing.length)
+      groupType.warmMany(missing)
+    }) 
+  })
 })
